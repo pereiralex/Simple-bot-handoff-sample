ce2f3a2ffc393a0362d8d6d28d9974afd6b06046
Alex Pereira <alex.pereira28@GMAIL.com>
2025-03-25 09:13:10 -0700

    Quickstart first run
    Got chat quickstart running

--- client.js
+++ client.js
@@ -2,8 +2,8 @@
 import { ChatClient, ChatThreadClient } from '@azure/communication-chat';
 import { AzureCommunicationTokenCredential } from '@azure/communication-common';
 
-let endpointUrl = 'https://<RESOURCE_NAME>.communication.azure.com';
-let userAccessToken = '<USER_ACCESS_TOKEN>';
+let endpointUrl = 'https://alexper-test1.unitedstates.communication.azure.com/';
+let userAccessToken = 'eyJhbGciOiJSUzI1NiIsImtpZCI6IkY1M0ZEODA0RThBNDhBQzg4Qjg3NTA3M0M4MzRCRDdGNzBCMzBENDUiLCJ4NXQiOiI5VF9ZQk9pa2lzaUxoMUJ6eURTOWYzQ3pEVVUiLCJ0eXAiOiJKV1QifQ.eyJza3lwZWlkIjoiYWNzOmMyZjJiZjU0LTFiMzctNDY3Zi1hZGUzLTE1YzY0MjhkMDMxMF8wMDAwMDAyNi02ZGU3LTUzNjgtZTEzOC04ZTNhMGQwMGM4OTEiLCJzY3AiOjE3OTIsImNzaSI6IjE3NDI5MTgyMzMiLCJleHAiOjE3NDMwMDQ2MzMsInJnbiI6ImFtZXIiLCJhY3NTY29wZSI6ImNoYXQiLCJyZXNvdXJjZUlkIjoiYzJmMmJmNTQtMWIzNy00NjdmLWFkZTMtMTVjNjQyOGQwMzEwIiwicmVzb3VyY2VMb2NhdGlvbiI6InVuaXRlZHN0YXRlcyIsImlhdCI6MTc0MjkxODIzM30.gZtIf6QFf-7oEX_2BGvVOCvn9ciWg0UY_jgVYav7MS_OAM0gNEap9sc_cW1O2XNQPqFckSPSeUhpbnWDQ5noyMB3Du7sdcS9tsqB7i8_doBgfsmBv09Ps_WWbZ_P_fOxZ0NECgeXeWJTtzyjWlBXOndWa31Foi84X9xtpjzH61U6NVNyduWeDwrAEwz-7uSWTNY68kQikJB1AcjN9eF-j1xW5cngCDkqR7Hi78HyelJa9-IPx33OTSiS5cVpM4PNqDxVnpN2LY8jcRf12lk0XL3oFisjk7pOppRC0Stxa7TYN2DMOYWxVj7fl4vPFb7Xjz4zsb0-CCMl3TX_37H4OQ';
 
 let chatClient = new ChatClient(endpointUrl, new AzureCommunicationTokenCredential(userAccessToken));
 console.log('Azure Communication Chat client created!');
@@ -16,8 +16,12 @@ async function createChatThread() {
     const createChatThreadOptions = {
         participants: [
             {
-                id: { communicationUserId: '<USER_ID>' },
-                displayName: '<USER_DISPLAY_NAME>'
+                id: { communicationUserId: '8:acs:c2f2bf54-1b37-467f-ade3-15c6428d0310_00000026-6de7-5368-e138-8e3a0d00c891' },
+                displayName: 'Mona Kane'
+            },
+            {
+                id: { communicationUserId: '8:acs:c2f2bf54-1b37-467f-ade3-15c6428d0310_00000026-6de8-ad3e-7137-8e3a0d00d703' },
+                displayName: 'John Smith'
             }
         ]
     };
@@ -73,7 +77,7 @@ createChatThread().then(async threadId => {
     {
         participants: [
             {
-                id: { communicationUserId: '<NEW_PARTICIPANT_USER_ID>' },
+                id: { communicationUserId: '8:acs:c2f2bf54-1b37-467f-ade3-15c6428d0310_00000026-6de7-5368-e138-8e3a0d00c891' },
                 displayName: 'Jane'
             }
         ]
@@ -87,7 +91,7 @@ createChatThread().then(async threadId => {
     }
 
     // <Remove user from a chat thread>
-    await chatThreadClient.removeParticipant({ communicationUserId: '<NEW_PARTICIPANT_USER_ID>' });
+    await chatThreadClient.removeParticipant({ communicationUserId: '8:acs:c2f2bf54-1b37-467f-ade3-15c6428d0310_00000026-6de7-5368-e138-8e3a0d00c891' });
     const users = chatThreadClient.listParticipants();
     for await (const user of users) {
         console.log(`participants in thread available:${user.id.communicationUserId}`);
